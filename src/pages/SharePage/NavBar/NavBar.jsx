2595ee46176bff69cc7e6a200908d06c1891d046
Saiful264 <saifulislamx264@gmail.com>
2023-05-03 12:44:56 +0600

    navbar is complete

--- src/pages/SharePage/NavBar/NavBar.jsx
+++ src/pages/SharePage/NavBar/NavBar.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo/chef-1.png";
 
 const NavBar = () => {
-    return (
-        <nav>
-            <div className='flex justify-between px-9'>
-                <Link className='uppercase font-bold'>Chef's Recipe</Link>
-                <ul className='flex'>
-                    <li>Home</li>
-                    <li>Blog</li>
-                    <li>LogIn</li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="flex justify-between items-center px-16 py-6 bg-slate-200">
+        <Link className="flex items-center gap-2">
+          <div className="flex items-center justify-center bg-cyan-200 h-12 w-12 rounded-full">
+            <img
+              className="w-7"
+              src={logo}
+              alt="logo"
+              srcset=""
+            />
+          </div>
+          <span className="uppercase font-bold text-2xl">Chef's Recipe</span>
+        </Link>
+        <ul className="flex font-bold gap-4">
+          <li>Home</li>
+          <li>Blog</li>
+          <li>LogIn</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
