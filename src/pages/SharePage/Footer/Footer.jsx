58d928015ee92a7ad004a6092df31ff5ec999a59
Saiful264 <saifulislamx264@gmail.com>
2023-05-03 17:15:35 +0600

    added full responsive navbar and footer

--- src/pages/SharePage/Footer/Footer.jsx
+++ src/pages/SharePage/Footer/Footer.jsx
@@ -1,11 +1,80 @@
-import React from 'react';
-
+import React from "react";
+import logo from "../../../assets/logo/chef-1.png";
+import { Link } from "react-router-dom";
 const Footer = () => {
-    return (
-        <div>
-            <h2>This is footer</h2>
+  return (
+    <div>
+      <div className="px-5 py-6 lg:px-24 lg:py-28  bg-gray-950 mt-28">
+        <div className="grid grid-cols lg:grid-cols-6 pb-10">
+          <div className="col-span-2">
+            <h3 className="text-white font-extrabold text-3xl pb-5">
+            <Link className="flex items-center gap-2">
+          <div className="flex items-center justify-center bg-cyan-200 h-12 w-12 rounded-full">
+            <img
+              className="w-7"
+              src={logo}
+              alt="logo"
+            />
+          </div>
+          <span className="uppercase font-bold text-2xl">Chef's Recipe</span>
+        </Link>
+            </h3>
+            <p className="text-slate-300 font-normal text-base w-5/6 pb-5">
+              There are many variations of passages of Lorem Ipsum , but the
+              majority have suffered alteration in some form.
+            </p>
+          </div>
+          <div>
+            <h1 className="text-slate-300 text-xl font-semibold pb-3">
+              Company
+            </h1>
+            <p className="text-slate-300 font-normal text-base">About</p>
+            <p className="text-slate-300 font-normal text-base">Us Work</p>
+            <p className="text-slate-300 font-normal text-base">Latest News</p>
+            <p className="text-slate-300 font-normal text-base">Careers</p>
+          </div>
+          <div>
+            <h1 className="text-slate-300 text-xl font-semibold pb-3">
+              Chef's
+            </h1>
+            <p className="text-slate-300 font-normal text-base">John Smith</p>
+            <p className="text-slate-300 font-normal text-base">
+            Maria Rodriguez
+            </p>
+            <p className="text-slate-300 font-normal text-base"> David Lee</p>
+            <p className="text-slate-300 font-normal text-base">Michael Chang</p>
+          </div>
+          <div>
+            <h1 className="text-slate-300 text-xl font-semibold pb-3">
+              Support
+            </h1>
+            <p className="text-slate-300 font-normal text-base">Help Desk</p>
+            <p className="text-slate-300 font-normal text-base">Sales</p>
+            <p className="text-slate-300 font-normal text-base">
+              Become a Partner
+            </p>
+            <p className="text-slate-300 font-normal text-base">Developers</p>
+          </div>
+          <div>
+            <h1 className="text-slate-300 text-xl font-semibold pb-3">
+              Contact
+            </h1>
+            <p className="text-slate-300 font-normal text-base">
+              524 Broadway , NYC
+            </p>
+            <p className="text-slate-300 font-normal text-base">
+              +1 777 - 978 - 5570
+            </p>
+          </div>
         </div>
-    );
+        <hr />
+        <div className="flex justify-between pt-11">
+          <p className="text-slate-400">@2023 CHEF'S RECIP. All Rights Reserved</p>
+          <p className="text-slate-400">Powered by CHEF'S RECIP</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/SharePage/NavBar/NavBar.jsx
+++ src/pages/SharePage/NavBar/NavBar.jsx
@@ -1,27 +1,126 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../../assets/logo/chef-1.png";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav>
-      <div className="flex justify-between items-center px-16 py-6 bg-slate-200">
-        <Link className="flex items-center gap-2">
+    <nav className=" px-4 py-5 sm:max-w-xl md:max-w-full lg:max-w-screen md:px-24 lg:px-16 bg-slate-200">
+      <div className="relative flex justify-between items-center ">
+        <Link to="/" className="flex items-center gap-2">
           <div className="flex items-center justify-center bg-cyan-200 h-12 w-12 rounded-full">
-            <img
-              className="w-7"
-              src={logo}
-              alt="logo"
-              srcset=""
-            />
+            <img className="w-7" src={logo} alt="logo" />
           </div>
           <span className="uppercase font-bold text-2xl">Chef's Recipe</span>
         </Link>
-        <ul className="flex font-bold gap-4">
-          <li>Home</li>
-          <li>Blog</li>
-          <li>LogIn</li>
+        <ul className="font-bold gap-9 hidden lg:flex">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "default:")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Login"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              LogIn
+            </NavLink>
+          </li>
         </ul>
+        <div className="lg:hidden">
+          <button
+            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            onClick={() => setIsOpen(true)}
+          >
+            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+              />
+            </svg>
+          </button>
+          {isOpen && (
+            <div className="absolute z-100 top-0 left-0 w-full p-2 rounded-md bg-slate-200">
+              <div className="flex items-center justify-between">
+                <Link to="/" className="flex items-center gap-2">
+                  <div className="flex items-center justify-center bg-cyan-200 h-12 w-12 rounded-full">
+                    <img className="w-7" src={logo} alt="logo" />
+                  </div>
+                  <span className="uppercase font-bold text-2xl">
+                    Chef's Recipe
+                  </span>
+                </Link>
+
+                <button
+                  className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-slate-400 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="pl-4 pt-3">
+              <ul className="font-bold gap-9">
+                <li>
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive ? "active" : "default:"
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/blog"
+                    className={({ isActive }) =>
+                      isActive ? "active" : "default"
+                    }
+                  >
+                    Blog
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Login"
+                    className={({ isActive }) =>
+                      isActive ? "active" : "default"
+                    }
+                  >
+                    LogIn
+                  </NavLink>
+                </li>
+              </ul>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
